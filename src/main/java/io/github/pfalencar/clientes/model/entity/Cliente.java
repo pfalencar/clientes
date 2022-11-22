ab6640e206ceb06503d93a06f90a626a238d9c39
patriciaalencar <patricia.alencar@quikdev.com.br>
2022-11-22 12:59:46 -0300

    Regras de validacao

--- src/main/java/io/github/pfalencar/clientes/model/entity/Cliente.java
+++ src/main/java/io/github/pfalencar/clientes/model/entity/Cliente.java
@@ -2,8 +2,11 @@ package io.github.pfalencar.clientes.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -19,9 +22,12 @@ public class Cliente {
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotEmpty
     private String nome;
 
     @Column(nullable = false, length = 11)
+    @NotNull
+    @CPF
     private String cpf;
 
     @Column(name = "data_cadastro", updatable = false)
